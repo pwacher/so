2431017cb6e4fefedeeab709dd87f5036f426903
Patrick Wacher <pwacher@gmail.com>
2014-06-14 16:33:58 -0700

    SO Site coding

---
+++ json_test.js
@@ -0,0 +1,175 @@
+var release = {
+status: "Accepted",
+styles: [
+"Electro"
+],
+series: [ ],
+released_formatted: "2006",
+labels: [
+{
+id: 2902,
+resource_url: "http://api.discogs.com/labels/2902",
+catno: "SO-010",
+name: "Southern Outpost",
+entity_type: "1"
+}
+],
+estimated_weight: 230,
+community: {
+status: "Accepted",
+rating: {
+count: 13,
+average: 4
+},
+have: 76,
+contributors: [
+{
+username: "bushwick",
+resource_url: "http://api.discogs.com/users/bushwick"
+},
+{
+username: "SoulDancer",
+resource_url: "http://api.discogs.com/users/SoulDancer"
+},
+{
+username: "cyrus.von.stern",
+resource_url: "http://api.discogs.com/users/cyrus.von.stern"
+},
+{
+username: "mayday",
+resource_url: "http://api.discogs.com/users/mayday"
+},
+{
+username: "lieuwe",
+resource_url: "http://api.discogs.com/users/lieuwe"
+}
+],
+want: 24,
+submitter: {
+username: "bushwick",
+resource_url: "http://api.discogs.com/users/bushwick"
+},
+data_quality: "Needs Vote"
+},
+released: "2006",
+year: 2006,
+images: [
+{
+uri: "http://api.discogs.com/image/R-634711-1141351455.jpeg",
+height: 450,
+width: 600,
+resource_url: "http://api.discogs.com/image/R-634711-1141351455.jpeg",
+type: "secondary",
+uri150: "http://api.discogs.com/image/R-150-634711-1141351455.jpeg"
+},
+{
+uri: "http://api.discogs.com/image/R-634711-1141351478.jpeg",
+height: 450,
+width: 600,
+resource_url: "http://api.discogs.com/image/R-634711-1141351478.jpeg",
+type: "secondary",
+uri150: "http://api.discogs.com/image/R-150-634711-1141351478.jpeg"
+}
+],
+format_quantity: 1,
+id: 634711,
+genres: [
+"Electronic"
+],
+thumb: "http://api.discogs.com/image/R-150-634711-1141351455.jpeg",
+extraartists: [
+{
+join: "",
+name: "Maneki.org",
+anv: "",
+tracks: "",
+role: "Design",
+resource_url: "http://api.discogs.com/artists/2410070",
+id: 2410070
+},
+{
+join: "",
+name: "Sentinel, The (2)",
+anv: "",
+tracks: "",
+role: "Producer, Written-By",
+resource_url: "http://api.discogs.com/artists/271428",
+id: 271428
+}
+],
+title: "Trust No One EP",
+country: "Australia",
+notes: "Distributed by: Clone.nl ",
+companies: [
+{
+name: "Clone Distribution",
+entity_type: "9",
+catno: "",
+resource_url: "http://api.discogs.com/labels/331677",
+id: 331677,
+entity_type_name: "Distributed By"
+}
+],
+uri: "http://www.discogs.com/Sentinel-Trust-No-One-EP/release/634711",
+artists: [
+{
+join: "",
+name: "Sentinel, The (2)",
+anv: "",
+tracks: "",
+role: "",
+resource_url: "http://api.discogs.com/artists/271428",
+id: 271428
+}
+],
+formats: [
+{
+descriptions: [
+"12"",
+"EP",
+"45 RPM"
+],
+name: "Vinyl",
+qty: "1"
+}
+],
+resource_url: "http://api.discogs.com/releases/634711",
+tracklist: [
+{
+duration: "",
+position: "A1",
+type_: "track",
+title: "Watch Your Back"
+},
+{
+duration: "",
+position: "A2",
+type_: "track",
+title: "WYB (Disintegration)"
+},
+{
+duration: "",
+position: "B1",
+type_: "track",
+extraartists: [
+{
+join: "",
+name: "Miss Memory",
+anv: "",
+tracks: "",
+role: "Mixed By",
+resource_url: "http://api.discogs.com/artists/209077",
+id: 209077
+}
+],
+title: "Wrong Time"
+},
+{
+duration: "",
+position: "B2",
+type_: "track",
+title: "Alles Klar?"
+}
+],
+data_quality: "Needs Vote"
+}
